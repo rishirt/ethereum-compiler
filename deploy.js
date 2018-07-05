015f2716099f444d1ca110aa53417238438aa06a
Rishi Thulasiraman <rishi@Rishis-MacBook-Pro.local>
2018-07-06 00:20:24 +0530

    update deploy script

--- deploy.js
+++ deploy.js
@@ -9,9 +9,23 @@ const provider = new HDWalletProvider(
 
 const web3 = new Web3(provider);
 
+const jsonInterface = JSON.parse(compile.interface);
+
 const deploy = async () => {
 	const accounts = await web3.eth.getAccounts();
 	console.log('attempting to deploy from the account:',accounts[0]);
+
+	const result = await new web3.eth.Contract(jsonInterface)
+	.deploy({
+		data : compile.bytecode ,
+		arguments : ['Hello World!']
+	})
+	.send({
+		gas : '1000000',
+		from : accounts[0]
+	});
+
+	console.log(result.options.address);
 };
 
 deploy();
